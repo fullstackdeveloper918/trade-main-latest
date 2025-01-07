a37d10b7697f689292402a81119f3f1d56f23937
fullstackdeveloper918 <34833947+fullstackdeveloper918@users.noreply.github.com>
2025-01-07 19:47:39 +0530

    Update Table.jsx

--- web/frontend/components/Table/Table.jsx
+++ web/frontend/components/Table/Table.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './Table.css';
 
 const Table = ({ data, headData }) => {
   return (
     <>
-      <table style={{ borderCollapse: "collapse", width: "100%" }}>
+      <table style={{ borderCollapse: "collapse" }} className="tablePadding">
         <thead>
           <tr>
             {headData.map((head) => (
@@ -24,11 +25,13 @@ const Table = ({ data, headData }) => {
               <tr key={index}>
                 <td>{item?.id
                 }</td>
+                     <td>{item?.shoipfy_order_id}</td>
+
                 <td>{item?.device_name}</td>
+
                 <td>{item?.specailPricce}</td>
                 <td>{item?.device_values}</td>
                 <td>{item?.type}</td>
-                <td>{item?.shoipfy_order_id}</td>
                 <td>{item?.wordpress_order_id}</td>
               </tr>
             ))
