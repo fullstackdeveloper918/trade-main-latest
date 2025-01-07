e61acf08fa6f4f343b097dbc0729ca3c4861491f
fullstackdeveloper918 <34833947+fullstackdeveloper918@users.noreply.github.com>
2025-01-07 16:40:15 +0530

    Update db.js

--- web/db.js
+++ web/db.js
@@ -3,10 +3,10 @@ import mysql from "mysql2";
 
 
 let db_con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "sellmac",
+  host: "srv1331.hstgr.io",
+  user: "u448961291_fullstack",
+  password: "#9usf4:g1oC",
+  database: "u448961291_sellmac_databa",
 });
 
 db_con.connect((err) => {
