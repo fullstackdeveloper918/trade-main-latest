04f581cb67dd721fca5e865655ca39abf31d7810
fullstackdeveloper918 <34833947+fullstackdeveloper918@users.noreply.github.com>
2025-01-07 18:57:20 +0530

    Update db.js

--- web/db.js
+++ web/db.js
@@ -1,19 +1,21 @@
 import mysql from "mysql2";
 
-
-
-let db_con = mysql.createConnection({
+let db_con = mysql.createPool({
   host: "srv1331.hstgr.io",
   user: "u448961291_fullstack",
   password: "#9usf4:g1oC",
   database: "u448961291_sellmac_databa",
+  waitForConnections: true,
+  connectionLimit: 10, // Maximum connections in the pool
+  queueLimit: 0, // Unlimited queue requests
 });
 
-db_con.connect((err) => {
+db_con.getConnection((err, connection) => {
   if (err) {
     console.log("Database Connection Failed !!!", err);
   } else {
     console.log("connected to Database");
+    connection.release(); // Release the connection back to the pool
   }
 });
 
